0835037e61dc1f6c340a1b7bb065fe2f3b5cd1ee
Artem D <artem.dudynskiy@gmail.com>
2018-12-19 01:10:15 +0200

    Fix data in posts actions tests

--- src/actions/__tests__/posts-actions-test.js
+++ src/actions/__tests__/posts-actions-test.js
@@ -8,52 +8,52 @@ import {
   fetchRefreshedPostsActions,
 } from '../postsActions';
 import {FETCH_POSTS, FETCH_MORE_POSTS, FETCH_REFRESHED_POSTS} from '../types';
-import {postsResponse} from '../../config/jest/mockData';
+import {postsResponse as posts} from '../../config/jest/mockData';
 
 const store = mockStore();
 const axios = require('axios');
 const MockAdapter = require('axios-mock-adapter');
 const mock = new MockAdapter(axios);
 
+const successResponse = {data: posts};
+const errorResponse = new Error('Look ma! I am an error.');
+
 beforeEach(() => {
   store.clearActions();
 });
 
 it('should handle ' + FETCH_POSTS.success + ' action', async () => {
-  mock.onGet().reply(200, postsResponse);
+  mock.onGet().reply(200, successResponse);
   await store.dispatch(fetchPosts(fetchPostsActions));
   expect(store.getActions()).toMatchSnapshot();
 });
 
 it('should handle ' + FETCH_POSTS.failure + ' action', async () => {
-  const mockError = new Error('Look ma! I am an error.');
-  mock.onGet().reply(404, mockError);
+  mock.onGet().reply(404, errorResponse);
   await store.dispatch(fetchPosts(fetchPostsActions));
   expect(store.getActions()).toMatchSnapshot();
 });
 
 it('should handle ' + FETCH_MORE_POSTS.success + ' action', async () => {
-  mock.onGet().reply(200, postsResponse);
+  mock.onGet().reply(200, successResponse);
   await store.dispatch(fetchMorePosts(fetchMorePostsActions));
   expect(store.getActions()).toMatchSnapshot();
 });
 
 it('should handle ' + FETCH_MORE_POSTS.failure + ' action', async () => {
-  const mockError = new Error('Look ma! I am an error.');
-  mock.onGet().reply(404, mockError);
+  mock.onGet().reply(404, errorResponse);
   await store.dispatch(fetchMorePosts(fetchMorePostsActions));
   expect(store.getActions()).toMatchSnapshot();
 });
 
 it('should handle ' + FETCH_REFRESHED_POSTS.success + ' action', async () => {
-  mock.onGet().reply(200, postsResponse);
+  mock.onGet().reply(200, successResponse);
   await store.dispatch(fetchRefreshedPosts(fetchRefreshedPostsActions));
   expect(store.getActions()).toMatchSnapshot();
 });
 
 it('should handle ' + FETCH_REFRESHED_POSTS.failure + ' action', async () => {
-  const mockError = new Error('Look ma! I am an error.');
-  mock.onGet().reply(404, mockError);
+  mock.onGet().reply(404, errorResponse);
   await store.dispatch(fetchRefreshedPosts(fetchRefreshedPostsActions));
   expect(store.getActions()).toMatchSnapshot();
 });
