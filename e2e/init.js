19bcd6f73111413bd41c1c1c4246846280012d29
Artem Dudinskyi <artem.dudynskiy@gmail.com>
2019-01-07 22:14:57 +0200

    fix release configuration for detox

--- e2e/init.js
+++ e2e/init.js
@@ -4,7 +4,7 @@ const detox = require('detox');
 const config = require('../package.json').detox;
 const adapter = require('detox/runners/jest/adapter');
 
-jest.setTimeout(240000);
+jest.setTimeout(120000);
 
 jasmine.getEnv().addReporter(adapter);
 
